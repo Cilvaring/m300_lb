e1792ca0f3f701a448781f260f531ee17bec80d8
unknown <cilvaring@gmail.com>
2021-03-16 15:06:03 +0100

    vagrant

---
+++ lb2/PlexVagrant/InstallPlex.sh
@@ -0,0 +1,5 @@
+sudo apt-get -y install curl
+echo "deb http://shell.ninthgate.se/packages/debian wheezy main" | sudo tee -a /etc/apt/sources.list.d/plexmediaserver.list
+sudo curl http://shell.ninthgate.se/packages/shell.ninthgate.se.gpg.key | sudo apt-key add -
+sudo apt-get -y update
+sudo apt-get -y install plexmediaserver
